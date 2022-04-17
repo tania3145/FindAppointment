093c18365a6143736dadea85f1e0ded94588ec7b
Tania Maria <tania.curutchi@gmail.com>
2022-04-17 16:38:31 +0300

    Added Google Maps styling and removed POI markers.

--- app/src/main/java/com/example/findappointment/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/findappointment/ui/home/HomeFragment.java
@@ -4,9 +4,11 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +26,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 
 import java.util.List;
 
@@ -34,22 +37,29 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        // Set location permissions.
         setPermissions();
+
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        binding.centerLocationFab.setOnClickListener(view -> centerUserLocation());
-        initMap();
+        // binding.centerLocationFab.setOnClickListener(view -> centerUserLocation());
+
+        // Initialise map.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         return root;
     }
 
     private void setPermissions() {
-        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(), new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION  }, 0);
+        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
         }
-        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
     }
@@ -71,16 +81,10 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
         return bestLocation;
     }
 
-    private void initMap() {
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
     private void centerUserLocation() {
         Location currentLocation = getLastKnownLocation();
         LatLng currentLocationLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocationLatLng,13.5f));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocationLatLng, 13.5f));
     }
 
     @SuppressLint("MissingPermission")
@@ -88,6 +92,14 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(@NonNull GoogleMap map) {
         this.map = map;
         map.setMyLocationEnabled(true);
+        try {
+            if (!this.map.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(getActivity(), R.raw.google_maps))) {
+                Log.e(getString(R.string.app_tag), "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(getString(R.string.app_tag), "Can't find style. Error: ", e);
+        }
         centerUserLocation();
     }
 }
